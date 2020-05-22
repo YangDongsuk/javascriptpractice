187bf502c11f8189f8109a49a4382e625aadd95c
dongsukyang <scienceyds@naver.com>
2020-05-22 21:39:13 +0900

    생활코딩 8~10

---
+++ color.js
@@ -0,0 +1,44 @@
+var Links={
+    SetColor:function(color){
+    // var alist=document.querySelectorAll('a');
+    //     var i =0;
+    //     while(i<alist.length){
+    //         alist[i].style.color=color;
+    //         i=i+1;
+    //     } 
+    $('a').css('color',color)
+}
+
+}
+var Body={
+    SetColor:function(color){
+    //document.querySelector('body').style.color=color;
+    $('body').css('color',color);
+
+},
+    SetBackgroundColor:function(color){
+    //document.querySelector('body').style.backgroundColor=color;
+    $('body').css('backgroundColor',color);
+
+}
+}
+
+function nightDayHandler(s){
+ 
+    if(s.value==='night'){
+        Body.SetBackgroundColor('black');
+        Body.SetColor('white');
+        s.value='day'
+        Links.SetColor('powderblue');
+
+
+    }
+    else{
+        Body.SetBackgroundColor('white');
+        Body.SetColor('black');
+        s.value='night'
+    
+        Links.SetColor('blue');
+
+    }
+}
